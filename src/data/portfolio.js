bf4cba75dd3efce681508b3087191cbf81381ac2
Alcindo Chavarria <139635361+onechavarria@users.noreply.github.com>
2026-09-20 00:39:44 -0300

    agregar correo personal

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -2,7 +2,7 @@ export const profile = {
   name: 'Alcindo Chavarría',
   role: 'Full Stack Developer',
   location: 'Chile',
-  email: 'onechavito1615@gmail.com',
+  email: 'alcindochavarria@gmail.com',
   githubUrl: 'https://github.com/onechavarria',
   linkedinUrl: 'https://www.linkedin.com/in/onechavarria',
 }
